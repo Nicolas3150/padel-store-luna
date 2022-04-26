35eb0cfa08c3e6f4dd963fe86d574b8d1958579d
Nicolas3150 <nicolasluna3150@gmail.com>
2022-04-26 14:57:30 -0300

    add counter with button

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './ItemCount.css'
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+    const [count, setCount] = useState(initial ? initial : 1);
+    const handleDecrease = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    }
+    const handleIncrease = () => {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+    return (
+        <div className='itemCount'>
+            <div className='buttonCount'>
+                <button onClick={() => handleDecrease()}> - </button>
+                <input value = { count } readOnly /> 
+                <button onClick={() => handleIncrease()}> + </button>
+            </div>
+            <button className='buttonAdd' onClick={() => (count <= stock) && onAdd(count)}>
+                Agregar al carrito
+            </button>
+        </div>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 import './ItemListContainer.css'
 
 const ItemListContainer = ({ greeting }) => {
+  const addCart = (count) => {
+    let message = `Agregaste ${count} producto`;
+    count === 1 ? alert(message) : alert(`${message}s`);
+  }
+
   return (
-    <div className='item-container'>{ greeting }</div>
+    <div className='item-container'>
+      { greeting }
+      <ItemCount stock={ 5 } initial={ 1 } onAdd={ addCart }/>
+    </div>
   )
 }
 
