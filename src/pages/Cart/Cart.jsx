559bc78d6ef00d0533e7e1cf389201fe08c3f472
Nicolas3150 <nicolasluna3150@gmail.com>
2022-05-17 21:26:46 -0300

    update Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CartContext from '../../store/CartContext'
+import ItemCart from '../../components/ItemCart/ItemCart'
 import './Cart.css'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
+  const cartCtx = useContext(CartContext);
+
   return (
-    <div className='cart'>Cart</div>
+    <div className='cart'>
+      {cartCtx.products.length ?
+        <>
+          <div className='cartTittle'>
+            <h3 className='tittleItem'>Item</h3>
+            <div className='tittleItemPCT'>
+              <h3>Precio</h3>
+              <h3 className='mount'>Cantidad</h3>
+              <h3 className='total'>Total</h3>
+            </div>
+          </div>
+          <div className='cart-item'>
+            {cartCtx.products.map((p) => <ItemCart item={p} key={'cartItem' + p.id} />)}
+            <h4>Total: ${cartCtx.getTotal()}</h4>
+            <hr className='divisor' />
+            <div className='buttonsContainer'>
+              <button className='btn' onClick={() => cartCtx.checkout()}>Finalizar compra</button>
+              <button className='btn-delete' onClick={() => cartCtx.clear()}>Eliminar carrito</button>
+            </div>
+          </div>
+        </> :
+        <div className='cart-item'>
+          <h2 className='no-items'>No hay items en el carrito</h2>
+          <Link to={'/'}>
+            <button className='btn-delete'>Ir al inicio...</button>
+          </Link>
+        </div>
+      }
+    </div>
   )
 }
 
